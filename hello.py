5f800e80f48c96b54ed2b29777fc60b1d50c7bd8
ylsi <623664140@qq.com>
2017-02-15 16:29:29 +0800

    add template

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/index')
+def index():
+	return 'Hello World'
+
+if __name__ == '__main__':
+	app.run(debug=True)
